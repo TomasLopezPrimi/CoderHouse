2007a728723b0f438593903cf82a9df045cf9087
Tomas Emanuel Lopez Primi <primi.tomas@hotmail.com>
2023-02-09 14:27:08 -0300

    commit

--- e-commerce/src/services/firebase/firebaseConfig.js
+++ e-commerce/src/services/firebase/firebaseConfig.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_authDomain,
+  apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
   storageBucket: process.env.REACT_APP_storageBucket,
